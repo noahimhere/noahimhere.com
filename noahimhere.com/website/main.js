5332f2681f2d10651edaa0b7d72bebf266043f7c
noahimhere <noahimhere@gmail.com>
2024-05-26 22:09:01 -0400

    tween setup

--- noahimhere.com/website/main.js
+++ noahimhere.com/website/main.js
@@ -1,12 +1,14 @@
 import "./style.css";
 
 import * as THREE from "three";
+import * as TWEEN from '@tweenjs/tween.js'
 var clock = new THREE.Clock();
 var pos = new THREE.Vector3(); // create once and reuse
 var mouseX = 0;
 var mouseY = 0;
-var oldX;
-var oldY;
+var oldX = 0;
+var oldY = 0;
+var tween;
 document.addEventListener("mousemove", function (evt) {
     mouseX = evt.clientX;
     mouseY = evt.clientY;
@@ -60,18 +62,21 @@ function init() {
   var pos = new THREE.Vector3((mouseX / window.innerWidth) * 800 - 400, -(mouseY / window.innerHeight) * 800 + 400, 0);
   pos = new THREE.Vector3((mouseX / window.innerWidth) * 800 - 400, -(mouseY / window.innerHeight) * 800 + 400, 0);
   pointer.position.set(pos.x, pos.y, pos.z);
+//   var tween = new TWEEN.Tween(pointer.position);
 }
 
 animate();
 
 function animate() {
+//   TWEEN.update();
   requestAnimationFrame(animate);
 
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.02;
-
+  
   
   pos = new THREE.Vector3((mouseX / window.innerWidth) * 800 - 400, -(mouseY / window.innerHeight) * 800 + 400, 0);
+  tween = new TWEEN.Tween(pointer.position);
   // vec.set(
   //   ( ClientX / window.innerWidth ) * 2 - 1,
   //   - ( CcreenY / window.innerHeight ) * 2 + 1,
@@ -86,8 +91,14 @@ function animate() {
 
   // pos.copy( camera.position ).add( vec.multiplyScalar( distance ) );
 //   if(isNaN(pos.x) && isNaN(pos.y) == false){
-  pointer.position.set(0, 0, 0);
-  pointer.position.lerpVectors(pointer.position, pos, 0.5);
+
+     pointer.position.set(pos.x, pos.y, pos.z);
+     camera.lookAt(pointer.position);
+    
+    oldX = mouseX;
+    oldY = mouseY;
+  
+  
 //   console.log("IT AINT NAN BOI")
 //   }
 //   else{
@@ -95,11 +106,9 @@ function animate() {
 //     console.log("its... nan?")
 //   }
 //   console.log(pointer.position);
-  console.log(clock.getElapsedTime());
+//   console.log(clock.getElapsedTime());
 
   camera.lookAt(pointer.position.x, pointer.position.y, pointer.position.z);
-  oldX = mouseX;
-  oldY = mouseY;
 
   renderer.render(scene, camera);
 }
