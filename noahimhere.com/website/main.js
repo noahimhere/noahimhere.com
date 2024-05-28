376ee1e70836a581eb44bef7263b40de00e1f43d
noahimhere <noahimhere@gmail.com>
2024-05-27 23:25:27 -0700

    transmission material magic, basic tuning, HDR capabilities

--- noahimhere.com/website/main.js
+++ noahimhere.com/website/main.js
@@ -4,6 +4,7 @@ import * as THREE from "three";
 import * as TWEEN from "@tweenjs/tween.js";
 import { Sky } from 'three/addons/objects/Sky.js';
 import { Water } from 'three/addons/objects/Water.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 var clock = new THREE.Clock();
 var pos = new THREE.Vector3(); // create once and reuse
 var mouseX = window.innerWidth / 2;
@@ -24,15 +25,28 @@ var geometry, material;
 var pointer;
 var centerpiece;
 
-init();
 
+const hdrEquirect = new RGBELoader()
+.setPath( 'textures/' )
+.load( 'spruit_sunrise_2k.hdr', function () {
+
+    hdrEquirect.mapping = THREE.EquirectangularReflectionMapping;
+
+} );
+init();
 function getmouse() {}
 function init() {
+  //BASIC STARTER STUFF
   sun = new THREE.Vector3();
   clock.start();
   clock.running = true;
-
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+  });
   scene = new THREE.Scene();
+  scene.background = hdrEquirect;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
   camera = new THREE.PerspectiveCamera(
     75,
@@ -42,9 +56,22 @@ function init() {
   );
   camera.position.z = 1000;
 
+  //CENTER MAIN
+
   const centerpieceo = new THREE.BoxGeometry(100,100,100);
-  const centerpiecem = new THREE.MeshStandardMaterial({
-    color:0xffffff,
+  const centerpiecem = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff,
+    metalness: 1,
+    roughness: 0.27,
+    ior: 1.5,
+    envMap: hdrEquirect,
+    envMapIntensity: 1,
+    transmission: 1, // use material.transmission for glass materials
+    specularIntensity: 1,
+    specularColor: 0xffffff,
+    opacity: 1,
+    side: THREE.DoubleSide,
+    transparent: true
   })
   centerpiece = new THREE.Mesh(centerpieceo, centerpiecem);
   centerpiece.rotation.x = 45;
@@ -52,18 +79,15 @@ function init() {
   centerpiece.position.y = 50;
   scene.add(centerpiece);
 
-  renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#bg"),
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
 
-  document.body.appendChild(renderer.domElement);
+  
 
-  const cursor = new THREE.SphereGeometry(12, 4, 2);
+  //CURSOR FOR CAMERA PLACEMENT
+
+  const cursor = new THREE.SphereGeometry(0, 4, 2);
   const cursorm = new THREE.MeshBasicMaterial({
     transparent: true,
-    opacity: 1,
-    wireframe:true,
+    opacity: 0,
   });
   pointer = new THREE.Mesh(cursor, cursorm);
   scene.add(pointer);
@@ -78,14 +102,15 @@ function init() {
     0
   );
   pointer.position.set(pos.x, pos.y, pos.z);
-  //   var tween = new TWEEN.Tween(pointer.position);
+
+  //WATER 
 
   const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
   water = new Water(
       waterGeometry,
       {
-          textureWidth: 512,
-          textureHeight: 512,
+          textureWidth: 2048,
+          textureHeight: 2048,
           waterNormals: new THREE.TextureLoader().load( 'textures/waternormals.jpg', function ( texture ) {
 
               texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
@@ -104,54 +129,28 @@ function init() {
 
   scene.add( water );
 
-  const sky = new Sky();
-  sky.scale.setScalar( 10000 );
-  scene.add( sky );
 
-  const skyUniforms = sky.material.uniforms;
 
-  skyUniforms[ 'turbidity' ].value = 10;
-  skyUniforms[ 'rayleigh' ].value = 3;
-  skyUniforms[ 'mieCoefficient' ].value = 0.005;
-  skyUniforms[ 'mieDirectionalG' ].value = 0.3;
-//   skyUniforms[ 'exposure' ].value = renderer.toneMappingExposure;
-
-  const parameters = {
-      elevation: 2,
-      azimuth: 220,
-  };
-
-  const pmremGenerator = new THREE.PMREMGenerator( renderer );
-  const sceneEnv = new THREE.Scene();
-
-  let renderTarget;
-  const uniforms = sky.material.uniforms;
-  function updateSun() {
-
-      const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
-      const theta = THREE.MathUtils.degToRad( parameters.azimuth );
-
-      sun.setFromSphericalCoords( 1, phi, theta );
-
-      sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
-      water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
-      uniforms[ 'sunPosition' ].value.copy( sun );
-
-      if ( renderTarget !== undefined ) renderTarget.dispose();
-
-      sceneEnv.add( sky );
-      renderTarget = pmremGenerator.fromScene( sceneEnv );
-      scene.add( sky );
-
-      scene.environment = renderTarget.texture;
-
-  }
 
   oldX = mouseX;
   oldY = mouseY;
 
-  updateSun();
-  renderer.toneMappingExposure = renderer.toneMappingExposure;
+
+  renderer.toneMappingExposure = 1;
+}
+
+function generateTexture() {
+
+    const canvas = document.createElement( 'canvas' );
+    canvas.width = 2;
+    canvas.height = 2;
+
+    const context = canvas.getContext( '2d' );
+    context.fillStyle = 'white';
+    context.fillRect( 0, 1, 2, 1 );
+
+    return canvas;
+
 }
 
 animate();
@@ -228,6 +227,5 @@ function animate() {
   camera.rotation.z = camera.rotation.z + (distx * speed) / -550;
   camtargetx =  pointer.position.x / -2.5;
   camera.position.x = camtargetx;
-  console.log(camtargetx);
   renderer.render(scene, camera);
 }
